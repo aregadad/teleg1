aacee9d827a839c40a2c5914b56c0227ba09e575
aregadad <biafleks@gmail.com>
2023-05-15 22:42:42 +0300

    rename variables

--- main.py
+++ main.py
@@ -22,30 +22,30 @@ def main():
     while True:
         try:
             print('\nЗапрос отправлен. Ждем ответ от сервера')
-            response = requests.get(
+            feedback_response = requests.get(
                 url, params=params, headers=headers, timeout=120)
-            response.raise_for_status()
-            payload = response.json()
-            if payload['status'] == 'found':
-                params['timestamp'] = payload['last_attempt_timestamp']
-                if payload['new_attempts'][0]['is_negative']:
+            feedback_response.raise_for_status()
+            feedback_payload = feedback_response.json()
+            if feedback_payload['status'] == 'found':
+                params['timestamp'] = feedback_payload['last_attempt_timestamp']
+                if feedback_payload['new_attempts'][0]['is_negative']:
                     message = f'''
-                    У вас проверили работу: {payload['new_attempts'][0]['lesson_title']}.
+                    У вас проверили работу: {feedback_payload['new_attempts'][0]['lesson_title']}.
                     К сожалению в работе нашлись ошибки.
-                    Ссылка: {payload['new_attempts'][0]['lesson_url']}
+                    Ссылка: {feedback_payload['new_attempts'][0]['lesson_url']}
                     '''
                 else:
                     message = f'''
-                    У вас проверили работу: {payload['new_attempts'][0]['lesson_title']}.
+                    У вас проверили работу: {feedback_payload['new_attempts'][0]['lesson_title']}.
                     Преподавателю всё понравилось. Можно приступать к следующему уроку!
-                    Ссылка: {payload['new_attempts'][0]['lesson_url']}
+                    Ссылка: {feedback_payload['new_attempts'][0]['lesson_url']}
                     '''
                 print('Ответ получен')
                 bot.send_message(chat_id=telegram_chat_id,
                                  text=cleandoc(message))
                 print('Сообщение в телеграм отправлено')
-            elif payload['status'] == 'timeout':
-                params['timestamp'] = payload['timestamp_to_request']
+            elif feedback_payload['status'] == 'timeout':
+                params['timestamp'] = feedback_payload['timestamp_to_request']
                 print('Нет ответа от сервера.')
         except requests.ReadTimeout:
             print('Нет ответа от сервера.')
